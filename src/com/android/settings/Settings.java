70231556dbd9e30e885b28a78ff36068273639c2
elpaablo <donopwhile1@gmail.com>
2023-11-01 06:02:47 +0000

    Introduce AlphaSettings

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -529,7 +529,7 @@ public class Settings extends SettingsActivity {
      */
     public static class OneHandedSettingsActivity extends SettingsActivity { /* empty */ }
 
-    public static class crDroidSettingsLayoutActivity extends SettingsActivity {}
+    public static class AlphaSettingsActivity extends SettingsActivity {}
 
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
 
